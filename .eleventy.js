6f2534ea80411bb001834da5ad4cac0c4523cd0b
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-02 19:19:38 -0700

    Styles layout of the blog.
    This makes a few changes that I was too lazy to split into multiple commits:
    1. Sets strict options on Liquid so that it complains when I misuse properties, rather than silently doing nothing.
        * This has the side effect of complaining about my sorting code. I'm not sure exactly how to write this as the 11ty examples do not work for me. Removed this for now, but I'll need to revisit in the future.
    2. Passes through CSS so it can be used in the output.
        * This also has the side-effecting of requiring each template format to be explicitly stated, so we are limited to markdown and Liquid right now.
    3. Added an `index.css` which includes all the required styles for the index page.
    4. Added `base.css` which contains "sane" styles for every page (basically just removes annoying styles included by default).
    5. Added `theme.css` to contain all the theme constants and assign them as variables on the root scope.
    6. Pulled out the header and footer into different fragments with a class as an argument.
        * I can't quite figure out how to make the class argument optional as this seems to be impossible for top-level options: https://github.com/Shopify/liquid/issues/1034
    
    CSS files all import each other, which is done at runtime and requires many network requests. In the future I'll probably bundle the CSS together into top-level page files to remove the need for extra network requests.
    
    CSS variables are prefixed with `--dwac-*` ("Devel Without A Cause"), so they don't conflict with anything else.
    
    After a lot of trial and error I found that Liquid include templates can have variables passed in by simply assinging them. Passing data in the `{% include %}` statement itself does not seem to work for me.
    
    I wanted to keep the fragment CSS files alongside them in `_includes/`, but this is not served at runtime. Once I introduce the CSS bundler, I can revisit the organization of fragment-specific CSS files.

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,18 @@
  * @see https://www.11ty.dev/docs/config/
  */
 
-module.exports = {
-    dir: {
-        input: 'www/',
-    },
+module.exports = function (config) {
+    // Process markdown and liquid templates.
+    // Pass through *.css files to the output directory.
+    config.setTemplateFormats(['md', 'liquid', 'css']);
+    config.setLiquidOptions({
+        strict_filters: true,
+        strict_variables: true,
+    });
+
+    return {
+        dir: {
+            input: 'www/',
+        },
+    };
 };
