94d2cab90549b5cc237b055965f0c1a405312335
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-06 21:01:30 -0700

    Switching index page and includes to Nunjucks.
    After using Liquid I found myself underwhelmed. It seems to be very inconsitent and appears to have multiple dialects. Even 11ty docs use include syntax which doesn't seem to work for me. Despite my best efforts, I still don't know how to use the sort function with a custom comparison.
    
    After playing around with Nunjucks, it seems to be very similar but more consistent and usable. Macros are effectively includes with named parameters, with some nice import syntax. I was also actually able to get `sort` to work.
    
    This only updates the index page, updates to posts and removal of Liquid is to follow.

--- .eleventy.js
+++ .eleventy.js
@@ -6,10 +6,13 @@
 module.exports = function (config) {
     // Process markdown and liquid templates.
     // Pass through *.css files to the output directory.
-    config.setTemplateFormats(['md', 'liquid', 'css']);
+    config.setTemplateFormats(['md', 'liquid', 'njk', 'css']);
     config.setLiquidOptions({
         strict_filters: true,
         strict_variables: true,
+    });
+    config.addFilter('date', (date, options) => {
+        return new Date(date).toLocaleDateString('en', options);
     });
 
     return {
