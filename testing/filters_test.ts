f0d71d75c1bf06721a5b9a0d4e51a0edc741d8b4
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-24 22:34:43 -0700

    Migrates `testing/filters_test.js` to TypeScript.
    Unfortunately covariance/contravariance is a bit of a challenge here, since the simple implementations of filters are not technically broad enough to include all the inputs allowed by the type interface. As a simple workaround, I hard-casted it to the specific type, but this should be improved in TS 4.0, when array types can have spreads.

---
+++ testing/filters_test.ts
@@ -0,0 +1,50 @@
+import { execFilter } from './filters';
+
+type FilterCallback = (err: Error|null, result?: string) => void;
+
+// Variance bites us for testing simple filters because most implementations of
+// a filter will be a subtype for a particular set of inputs, rather than
+// handling all the intermixed cases of string params and callbacks in any order
+// as is specially required by the type. So instead, we can cast to a `Filter`
+// type to make the type checker happy without being too hacky.
+// TODO: TS 4.0 should allow more specific array types and can probably fix it.
+type Filter = Parameters<typeof execFilter>[0];
+
+describe('filters', () => {
+    describe('execFilter()', () => {
+        it('executes the given filter and resolves with the result',
+                async () => {
+            function testFilter(
+                    first: string, second: string, callback: FilterCallback) {
+                callback(null, first + second);
+            }
+
+            const result = await execFilter(
+                    testFilter as Filter, 'first', 'second');
+            
+            expect(result).toBe('firstsecond');
+        });
+
+        it('executes the given filter and rejects on a called back error',
+                async () => {
+            const err = new Error('Filter got clogged.');
+            function testFilter(_: string, callback: FilterCallback) {
+                callback(err);
+            }
+
+            await expectAsync(execFilter(testFilter as Filter, 'unused arg'))
+                    .toBeRejectedWith(err);
+        });
+
+        it('executes the given filter and rejects on a thrown error',
+                async () => {
+            const err = new Error('Filter got clogged.');
+            function testFilter() {
+                throw err;
+            }
+
+            await expectAsync(execFilter(testFilter, 'unused arg'))
+                    .toBeRejectedWith(err);
+        });
+    });
+});
