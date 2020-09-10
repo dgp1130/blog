eeea93f676cc08189b31ba84ed7697819c017d37
Doug Parker <dgp1130@users.noreply.github.com>
2020-09-09 18:57:37 -0700

    Adds `share` utility.
    This module provides functionality related to sharing. Currently this is simply making a URL path shareable. This resolves the URL to the current domain of the window and drops all non-shareable data (such as query parameters, hashes, username, etc.)

---
+++ src/client/share_test.ts
@@ -0,0 +1,27 @@
+import * as browserEnv from './browser_env';
+import { makeShareable } from "./share";
+
+describe('share', () => {
+    describe('makeShareable()', () => {
+        it('returns a URL from the current window origin', () => {
+            spyOn(browserEnv, 'getLocation').and.returnValue({
+                href: 'https://example.com/',
+            } as Location);
+
+            const url = makeShareable('/foo/bar/baz');
+
+            expect(url.toString()).toBe('https://example.com/foo/bar/baz');
+        });
+
+        it('returns a URL with non-shareable information santitized from the current window origin', () => {
+            spyOn(browserEnv, 'getLocation').and.returnValue({
+                // Most of this information isn't shareable.
+                href: 'https://user:pass@example.com:80/test?foo=bar#something',
+            } as Location);
+
+            const url = makeShareable('/foo/bar/baz');
+
+            expect(url.toString()).toBe('https://example.com:80/foo/bar/baz');
+        });
+    });
+});
