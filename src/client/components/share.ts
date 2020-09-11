51977e369380557704b9b447241ca1aff419b1ee
Doug Parker <dgp1130@users.noreply.github.com>
2020-09-10 18:34:59 -0700

    Adds `dwac-share` component.
    This provides sharing functionality via a "share" button which triggers the user agent's share API as well as a "copy" button which simply copies to the clipboard.
    
    This component supports its inputs coming from attributes, so it can be generated with static inputs at build time.
    
    Follow up work to come:
    * CSS styling for the component (just the implementation is here for now).
    * Direct integration with Twitter when `navigator.share()` is unsupported.
    * A small toast message to tell the user that the link was copied to the clipboard.

---
+++ src/client/components/share.ts
@@ -0,0 +1,67 @@
+import { LitElement, TemplateResult, html, property, customElement } from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { assertDefined } from '../asserts';
+import { makeShareable } from '../share';
+
+/** Displays a share UI to allow users to easily share the given URL. */
+@customElement('dwac-share')
+export class Share extends LitElement {
+    // Required.
+    @property({
+        converter: {
+            // Parse the string attribute into a URL object.
+            fromAttribute: (target) => {
+                if (!target) return null;
+
+                if (target.startsWith('/')) {
+                    return makeShareable(target);
+                } else {
+                    return new URL(target);
+                }
+            },
+        },
+    }) public target?: URL;
+
+    // Required.
+    @property({ attribute: 'article-title' }) public articleTitle?: string;
+
+    render(): TemplateResult|void {
+        return html`
+            ${ifDefined(navigator.share && html`
+                <button id="share" @click="${this.onShare.bind(this)}">
+                    Share!
+                </button>
+            `)}
+            ${ifDefined(navigator.clipboard?.writeText && html`
+                <button id="copy" @click="${this.onCopy.bind(this)}">
+                    Copy!
+                </button>
+            `)}
+        `;
+    }
+
+    /** Shares the URL via the user-agent. */
+    private async onShare(): Promise<void> {
+        assertDefined(this.target);
+        assertDefined(this.articleTitle);
+
+        await navigator.share({
+            title: this.articleTitle,
+            text: `Check out: "${this.articleTitle}".`,
+            url: this.target.toString(),
+        });
+    }
+
+    /** Copies the URL to the user's clipboard. */
+    private async onCopy(): Promise<void> {
+        assertDefined(this.target);
+
+        await navigator.clipboard.writeText(this.target.toString());
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'dwac-share': Share;
+    }
+}
---
+++ src/client/components/share_test.ts
@@ -0,0 +1,159 @@
+import * as browserEnv from '../browser_env';
+import { Share } from "./share";
+
+describe('Share', () => {
+    let share: Share|undefined;
+
+    beforeAll(() => {
+        // Make fake share function if not present in the browser.
+        navigator.share = navigator.share ?? new Function();
+    });
+
+    async function init({
+        target = new URL('https://dummy.url/'),
+        articleTitle = 'Dummy Article',
+    }: {
+        target?: URL | string,
+        articleTitle?: string,
+    } = {}): Promise<Share> {
+        share = new Share();
+        
+        // Either set the target property directly as a URL, or set an attribute
+        // which gets converted into a URL using the `fromAttribute()` function.
+        if (target instanceof URL) {
+            share.target = target;
+        } else {
+            share.setAttribute('target', target);
+        }
+
+        share.articleTitle = articleTitle;
+        document.body.appendChild(share);
+        
+        await share.updateComplete;
+        return share;
+    }
+
+    afterEach(() => {
+        share?.parentElement?.removeChild(share);
+    });
+
+    it('is defined', async () => {
+        const share = await init();
+
+        expect(share.tagName).toBe('DWAC-SHARE');
+    });
+
+    it('shows share UI when supported', async () => {
+        spyOn(navigator, 'share');
+
+        const share = await init();
+        
+        expect(navigator.share).not.toHaveBeenCalled();
+
+        expect(share.shadowRoot!.querySelector('#share')).not.toBeNull();
+    });
+
+    it('shares the given full URL when the user clicks "Share"', async () => {
+        spyOn(navigator, 'share').and.returnValue(Promise.resolve());
+
+        const share = await init({
+            target: new URL('https://shared.test/'),
+            articleTitle: 'Shared Article',
+        });
+
+        const shareBtn = share.shadowRoot!.querySelector('#share') as HTMLButtonElement;
+        expect(shareBtn).not.toBeNull();
+
+        shareBtn.click();
+        await share.updateComplete;
+
+        expect(navigator.share).toHaveBeenCalledWith({
+            title: 'Shared Article',
+            text: 'Check out: "Shared Article".',
+            url: 'https://shared.test/',
+        });
+    });
+
+    it('shares the given path when the user clicks "Share"', async () => {
+        spyOn(browserEnv, 'getLocation').and.returnValue({
+            href: 'https://shared.test/',
+        } as Location);
+        spyOn(navigator, 'share').and.returnValue(Promise.resolve());
+
+        const share = await init({
+            target: '/foo',
+            articleTitle: 'Shared Article',
+        });
+
+        const shareBtn = share.shadowRoot!.querySelector('#share') as HTMLButtonElement;
+        expect(shareBtn).not.toBeNull();
+
+        shareBtn.click();
+        await share.updateComplete;
+
+        expect(navigator.share).toHaveBeenCalledWith({
+            title: 'Shared Article',
+            text: 'Check out: "Shared Article".',
+            url: 'https://shared.test/foo',
+        });
+    });
+
+    it('shows copy UI when supported', async () => {
+        const clipboard = jasmine.createSpyObj('clipboard', {
+            writeText: () => {},
+        });
+        spyOnProperty(navigator, 'clipboard', 'get')
+                .and.returnValue(clipboard);
+        
+        const share = await init();
+
+        expect(navigator.clipboard.writeText).not.toHaveBeenCalled();
+
+        expect(share.shadowRoot!.querySelector('#copy')).not.toBeNull();
+    });
+
+    it('copies the given full URL when the user presses "Copy"', async () => {
+        const clipboard = jasmine.createSpyObj('clipboard', {
+            writeText: () => {},
+        });
+        spyOnProperty(navigator, 'clipboard', 'get')
+                .and.returnValue(clipboard);
+        
+        const share = await init({
+            target: new URL('https://copied.test/'),
+        });
+
+        const copyBtn = share.shadowRoot!.querySelector('#copy') as HTMLButtonElement;
+        expect(copyBtn).not.toBeNull();
+        
+        copyBtn.click();
+        await share.updateComplete;
+
+        expect(navigator.clipboard.writeText)
+                .toHaveBeenCalledWith('https://copied.test/');
+    });
+
+    it('copies the given path when the user presses "Copy"', async () => {
+        spyOn(browserEnv, 'getLocation').and.returnValue({
+            href: 'https://copied.test/',
+        } as Location);
+        const clipboard = jasmine.createSpyObj('clipboard', {
+            writeText: () => {},
+        });
+        spyOnProperty(navigator, 'clipboard', 'get')
+                .and.returnValue(clipboard);
+        
+        const share = await init({
+            target: '/foo',
+        });
+
+        const copyBtn = share.shadowRoot!.querySelector('#copy') as HTMLButtonElement;
+        expect(copyBtn).not.toBeNull();
+        
+        copyBtn.click();
+        await share.updateComplete;
+
+        expect(navigator.clipboard.writeText)
+                .toHaveBeenCalledWith('https://copied.test/foo');
+    });
+});
