ea4716f78c356bd712d2d186ea0d46bee9f0fbe7
Doug Parker <dgp1130@users.noreply.github.com>
2020-09-11 17:21:18 -0700

    Adds Twitter logo to share button.
    This sets the `<title></title>` tag of the SVG to act as an `alt` attribute.
    
    I also used `title` attribute for a simple tooltip, though that is apparently bad for a11y, so I'll need to revisit this in the future at some point. See https://inclusive-components.design/tooltips-toggletips/.

--- src/client/components/share.ts
+++ src/client/components/share.ts
@@ -1,4 +1,4 @@
-import { LitElement, TemplateResult, html, property, customElement } from 'lit-element';
+import { LitElement, TemplateResult, html, property, customElement, css } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined';
 import { assertDefined } from '../asserts';
 import { makeShareable } from '../share';
@@ -6,6 +6,21 @@ import { makeShareable } from '../share';
 /** Displays a share UI to allow users to easily share the given URL. */
 @customElement('dwac-share')
 export class Share extends LitElement {
+    static styles = css`
+        :host {
+            --dwac-twitter-blue: rgb(29, 161, 242);
+        }
+
+        #twitter-logo {
+            width: 24px;
+            height: 24px;
+            vertical-align: middle;
+
+            fill: currentColor;
+            color: var(--dwac-twitter-blue);
+        }
+    `;
+
     // Required.
     @property({
         converter: {
@@ -44,8 +59,12 @@ export class Share extends LitElement {
                     encodeURIComponent(`Check out: "${this.articleTitle}". ${
                             this.target.toString()}`)}"
                     target="_blank"
-                    rel="noopener">
-                Tweet!
+                    rel="noopener"
+                    title="Share on Twitter">
+                <svg id="twitter-logo">
+                    <title>Twitter logo</title>
+                    <g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g>
+                </svg>
             </a>
         `;
     }
