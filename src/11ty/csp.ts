bbb82eaa7c4a1692ce8634131467f61b32b729e0
Doug Parker <dgp1130@users.noreply.github.com>
2020-10-14 18:30:59 -0700

    Adds content security policy injector.
    This module is responsible for parsing an HTML file and extracting all the CSP-relevant resources to generate a policy string. This then gets injected back into the HTML enforce the policy.
    
    This strategy was inspired by: https://github.com/google/eleventy-high-performance-blog/blob/05f21022933495ce81c46da614b9ddfeceadfd62/_11ty/apply-csp.js
    
    I looked around for a proper "generate CSP from HTML" library. However the only options I found either processed an entire web site (which is really cool and totally overkill for my use case and the context of an 11ty post-processor), or worked at the file-level and not a string like I have. I decided to author my own which is good enough for my use case here and I may separate it out into its own library depending on how it works out here.
    
    I also explored a few different HTML parsing libraries. Some didn't have a means of injecting an element into them. Others didn't support `querySelector()` very well or were hard to use. Another returned data via callbacks making it very difficult to call synchronously. In the end, JsDom gave all the features I needed. It's a bit overkill for this use case, as it can do things like execute JavaScript and fake a rendering engine, none of which I need. However I still found this to be the most usable Node HTML manipulation library.
    
    The actual CSP generation just looks at all the HTML resources and extracts hashes for inlined resources or URLs for referenced resources. It also includes `object-src 'none';` by default, which is a good practice. There are undoubtly areas for improvement here, but this is sufficient for the current state of the blog. Over time it will probably need to be expanded to more comprehensively cover every CSP use case.

---
+++ src/11ty/csp.ts
@@ -0,0 +1,205 @@
+import { JSDOM as JsDom } from 'jsdom';
+import { createHash } from 'crypto';
+
+/**
+ * Injects a Content Security Policy into the given HTML string.
+ * 
+ * This parses the HTML document and identifies all the resources loaded by the
+ * page. It then generates the strictest CSP it can for the page, using file
+ * hashes for inline resources and URLs for hyperlinked resources.
+ * 
+ * IMPORTANT SECURITY NOTE: This function assumes that all resources on the page
+ * are trusted. Think very carefully before running on a page with user
+ * generated content.
+ * 
+ * @param html The HTML content to extract a policy from.
+ * @param scriptSrc Custom sources to add to the `script-src` policy.
+ * @returns The HTML content given, with a CSP `<meta />` tag injected into it.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+ */
+export function injectCsp(html: string, { scriptSrc = [] }: {
+    scriptSrc?: string[],
+} = {}): string {
+    // Parse the HTML into a manipulatable document.
+    const dom = new JsDom(html);
+    const document = dom.window.document;
+
+    // Extract sources from the document.
+    const scriptSources = Array.from(normalize(
+            scriptSrc.concat(Array.from(genScriptSources(document)))));
+    const styleSources = Array.from(normalize(genStyleSources(document)));
+    const imageSources = Array.from(normalize(genImageSources(document)));
+
+    // Generate a minimal policy string.
+    const csp = [
+        genDirective('script-src', scriptSources),
+        genDirective('style-src', styleSources),
+        genDirective('img-src', imageSources),
+        genDirective('object-src', [`'none'`]),
+    ].filter((directive) => !!directive).join(' ');
+
+    // Inject the CSP into the HTML.
+    const cspNode = document.createElement('meta');
+    cspNode.setAttribute('http-equiv', 'Content-Security-Policy');
+    cspNode.setAttribute('content', csp);
+    // CSP node must come **before** other scripts!
+    // Resources that preceed the CSP may not be appropriately blocked.
+    document.head.prepend(cspNode);
+
+    // Return the document modified with a new CSP value.
+    return dom.serialize();
+}
+
+/**
+ * Returns a CSP directive based on the given name and sources list. Returns
+ * `undefined` if the sources list is empty.
+ */
+function genDirective(directive: string, sources: string[]): string|undefined {
+    if (sources.length === 0) return undefined;
+    return `${directive} ${sources.join(' ')};`;
+}
+
+/** Yields sources for the `script-src` directive from the given document. */
+function* genScriptSources(document: Document): Iterable<string> {
+    // Get all JavaScript `<script />` tags.
+    const scriptTags = Array.from(document.querySelectorAll('script'))
+            .filter((script) => {
+                const type = script.getAttribute('type');
+
+                // Bare `<script />` tags are JavaScript.
+                if (!type) return true;
+
+                // `<script type="module" />` are JavaScript.
+                if (type === 'module') return true;
+
+                // Known MIME types are JavaScript, unknown are unrelated
+                // `<script />` tags.
+                return isJsMimeType(type);
+            });
+    
+    for (const scriptTag of scriptTags) {
+        // For a hyperlinked script, yield the source link.
+        const src = scriptTag.getAttribute('src');
+        if (src) yield src;
+
+        // For an inline script, yield a hash of its contents.
+        const { textContent } = scriptTag;
+        if (!!textContent && textContent !== '') yield hash(textContent);
+    }
+}
+
+/** Yields sources for the `style-src` directive from the given document. */
+function* genStyleSources(document: Document): Iterable<string> {
+    const styles = document.querySelectorAll('style');
+
+    // For an inlined style tag, yield a hash of its contents.
+    for (const style of styles) {
+        const { textContent } = style;
+        if (textContent) yield hash(textContent);
+    }
+
+    // For a hyperlinked source, yield the source link.
+    const links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (const link of links) {
+        const href = link.getAttribute('href');
+        if (href) yield href;
+    }
+}
+
+/** Yields sources for the `img-src` directive from the given document. */
+function* genImageSources(document: Document): Iterable<string> {
+    const images = document.querySelectorAll('img');
+    for (const image of images) {
+        // For a hyperlinked source, yield the source link.
+        const { src } = image;
+        if (src) yield src;
+    }
+}
+
+/**
+ * Normalizes the given source list. This performs a few operations:
+ * 1. Replaces self-hosted URLs with the 'self' source.
+ * 2. Removes query parameters from URLs (as they are unsupported by CSP).
+ * 3. Deduplicates sources.
+ */
+function normalize(sources: Iterable<string>): Iterable<string> {
+    const selfNormalized = normalizeSelf(sources);
+    const urlNormalized = normalizeUrls(selfNormalized)
+
+    return unique(urlNormalized);
+}
+
+/** Replaces all self-hosted sources with the 'self' source. */
+function* normalizeSelf(sources: Iterable<string>): Iterable<string> {
+    for (const source of sources) {
+        if (source.startsWith(`'`)) {
+            // Quoted sources are not URLs, ignore them.
+            yield source;
+            continue;
+        }
+
+        // Check if the URL does not have a domain by giving it a base. If the
+        // base is used, then the URL must not have a domain and must be
+        // self-hosted.
+        const url = new URL(source, 'http://arbitrary-url-for-csp.dne/');
+        if (url.hostname === 'arbitrary-url-for-csp.dne') {
+            yield `'self'`; // Self-hosted URL.
+        } else {
+            yield url.toString(); // Foreign URL.
+        }
+    }
+}
+
+/**
+ * Removes query parameters from source URLs. Query parameters are not supported
+ * on CSP strings.
+ */
+function* normalizeUrls(sources: Iterable<string>): Iterable<string> {
+    for (const source of sources) {
+        if (source.startsWith(`'`)) {
+            // Quoted sources are not URLs, ignore them.
+            yield source;
+            continue;
+        }
+
+        // Remove the query parameter.
+        const url = new URL(source);
+        url.search = '';
+        yield url.toString();
+    }
+}
+
+/** Removes duplicates from the input list. */
+function unique<T>(list: Iterable<T>): Iterable<T> {
+    return Array.from(new Set(list));
+}
+
+/** Calculates the CSP hash source for the given string. */
+function hash(text: string): string {
+    const hash = createHash('sha256').update(text).digest('base64');
+    return `'sha256-${hash}'`;
+}
+
+/** Returns whether or not the given MIME type is well-known JavaScript. */
+function isJsMimeType(type: string): boolean {
+    return jsMimeTypes.has(type);
+}
+
+const jsMimeTypes = new Set([
+    'application/javascript',
+    'application/ecmascript',
+    'application/x-ecmascript',
+    'application/x-javascript',
+    'text/javascript',
+    'text/ecmascript',
+    'text/javascript1.0',
+    'text/javascript1.1',
+    'text/javascript1.2',
+    'text/javascript1.3',
+    'text/javascript1.4',
+    'text/javascript1.5',
+    'text/jscript',
+    'text/livescript',
+    'text/x-ecmascript',
+    'text/x-javascript',
+]);
---
+++ src/11ty/csp_test.ts
@@ -0,0 +1,268 @@
+import { JSDOM as JsDom } from 'jsdom';
+import { injectCsp } from './csp';
+
+describe('csp', () => {
+    describe('injectCsp()', () => {
+        it('injects an empty policy', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head></head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`object-src 'none';`);
+        });
+
+        it('hashes inline <script /> tags', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script>'bare';</script>
+                        <script type="module">'module';</script>
+                        <script type="text/javascript">'test/javascript';</script>
+                        <script type="application/javascript">'application/javascript';</script>
+                        <script type="application/x-other">Not JavaScript</script>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            // All JavaScript tags should be hashed. `application/x-other` is
+            // not JavaScript by MIME type and should **not** be included.
+            expect(csp).toBe(`script-src 'sha256-5d34NEinNl1U1YeOFHTka8dTqARzL8+ETrmCJOaj5ZI=' 'sha256-QWTfWzCIE/oRx2c0Kop7Gc3AmbSFCLRt0DNf0mrUyCg=' 'sha256-ToLbmSfoSC+QpKNHjHHDuUHYqToDYeJau6UVYJMaC68=' 'sha256-q3H+UaiJ2s+2C6FQ2ZCmmLcY5ZKXaYrG/4h8lwUYX1s='; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, relative URL scripts', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="relative.js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`script-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, absolute path URL scripts', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="/absolute.js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`script-src 'self'; object-src 'none';`);
+        });
+
+        it('deduplicates multiple `self` script sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="relative.js"></script>
+                        <script src="/absolute.js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            // Only one 'self' should be included.
+            expect(csp).toBe(`script-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists foreign domain script sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="http://example1.com/js"></script>
+                        <script src="https://example2.com/js"></script>
+                        <script src="http://example3.com:8080/js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`script-src http://example1.com/js https://example2.com/js http://example3.com:8080/js; object-src 'none';`);
+        });
+
+        it('appends provided script sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="http://example2.com/js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `, { scriptSrc: ['http://example1.com/js', `'sha256-abc123'`] }));
+
+            expect(csp).toBe(`script-src http://example1.com/js 'sha256-abc123' http://example2.com/js; object-src 'none';`)
+        });
+
+        it('hashes inline <style /> sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <style>.foo{}</style>
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`style-src 'sha256-5gDzvFAxDTqq9+8IbIV1n6QT8rTTzWFgoqVBG94Zwl0='; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, relative URL style sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <link rel="stylesheet" href="relative.css">
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`style-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, absolute path URL style sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <link rel="stylesheet" href="/absolute.css">
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`style-src 'self'; object-src 'none';`);
+        });
+
+        it('deduplicates multiple `self` style sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <link rel="stylesheet" href="relative.css">
+                        <link rel="stylesheet" href="/absolute.css">
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`style-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists foreign domain style sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <link rel="stylesheet" href="http://example1.com/css">
+                        <link rel="stylesheet" href="https://example2.com/css">
+                        <link rel="stylesheet" href="http://example3.com:8080/css">
+                    </head>
+                    <body></body>
+                </html>
+            `));
+
+            expect(csp).toBe(`style-src http://example1.com/css https://example2.com/css http://example3.com:8080/css; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, relative URL image sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head></head>
+                    <body>
+                        <img src="relative.jpg" />
+                    </body>
+                </html>
+            `));
+
+            expect(csp).toBe(`img-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists self-hosted, absolute path URL image sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head></head>
+                    <body>
+                        <img src="/absolute.jpg" />
+                    </body>
+                </html>
+            `));
+
+            expect(csp).toBe(`img-src 'self'; object-src 'none';`);
+        });
+
+        it('deduplicates multiple `self` image sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head></head>
+                    <body>
+                        <img src="relative.jpg" />
+                        <img src="/absolute.jpg" />
+                    </body>
+                </html>
+            `));
+
+            expect(csp).toBe(`img-src 'self'; object-src 'none';`);
+        });
+
+        it('allowlists foreign domain image sources', () => {
+            const csp = getInjectedCsp(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head></head>
+                    <body>
+                        <img src="http://example1.com/css" />
+                        <img src="https://example2.com/css" />
+                        <img src="http://example3.com:8080/css" />
+                    </body>
+                </html>
+            `));
+
+            expect(csp).toBe(`img-src http://example1.com/css https://example2.com/css http://example3.com:8080/css; object-src 'none';`);
+        });
+
+        it('injects the policy as the first child of <head />', () => {
+            const { document } = new JsDom(injectCsp(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <script src="/foo.js"></script>
+                    </head>
+                    <body></body>
+                </html>
+            `)).window;
+
+            const firstChild = document.head.children[0];
+            expect(firstChild.tagName).toBe('META');
+            expect(firstChild.getAttribute('http-equiv'))
+                    .toBe('Content-Security-Policy');
+        });
+    });
+});
+
+function getInjectedCsp(html: string): string|undefined {
+    const { document } = new JsDom(html).window;
+    const meta = document.querySelector(
+        'meta[http-equiv="Content-Security-Policy"]');
+    return meta?.getAttribute('content') ?? undefined;
+}
