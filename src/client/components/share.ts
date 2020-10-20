a8289254e801edd0615ac6bd50b448730fbc7f2e
Doug Parker <dgp1130@users.noreply.github.com>
2020-10-19 18:25:30 -0700

    Enlarges share icons.
    Previously, share icons were only 24px tall, which can be hard to accurately touch on a mobile device. Recommendations vary, but a simple rule of thumb is that it should be at least 44px x 44px. https://medium.com/@zacdicko/size-matters-accessibility-and-touch-targets-56e942adc0cc#:~:text=In%20the%20Human%20Interface%20Guidelines,be%20trying%20to%20tap%20successfully.
    
    I scaled up the images to match that and spaced them out/line wrapped them to look aesthetically pleasing. These images are not square and only the height is fixed to 44px, so technically the clipboard icon is below the minimum width. I'll probably need to expand the tap target beyond the image dimensions to look good and still meet the touch target requirements, but I'll come to that later.

--- src/client/components/share.ts
+++ src/client/components/share.ts
@@ -13,8 +13,25 @@ export class Share extends LitElement {
             display: block;
         }
 
+        ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        span + ul {
+            margin-top: 10px;
+        }
+
+        li {
+            display: inline-block;
+        }
+
+        li + li {
+            margin-left: 5px;
+        }
+
         #share {
-            height: 24px;
+            height: 44px;
             vertical-align: middle;
         }
 
@@ -23,7 +40,7 @@ export class Share extends LitElement {
         }
 
         #copy {
-            height: 24px;
+            height: 44px;
             vertical-align: middle;
         }
 
@@ -32,8 +49,8 @@ export class Share extends LitElement {
         }
 
         #twitter-logo {
-            width: 24px;
-            height: 24px;
+            width: 44px;
+            height: 44px;
             vertical-align: middle;
 
             fill: currentColor;
@@ -89,27 +106,35 @@ export class Share extends LitElement {
             ${ifDefined(this.prompt && html`
                 <span>${this.prompt}</span>
             `)}
-
-            ${ifDefined(navigator.share && html`
-                <button id="share" @click="${this.onShare.bind(this)}"
-                        class="unstyled" title="Share">
-                    ${shareIcon}
-                </button>
-            `)}
-            ${ifDefined(navigator.clipboard?.writeText && html`
-                <button id="copy" @click="${this.onCopy.bind(this)}"
-                        class="unstyled" title="Copy to clipboard">
-                    ${copyIcon}
-                </button>
-            `)}
-            <a href="https://twitter.com/intent/tweet?text=${
-                    encodeURIComponent(`Check out: "${this.articleTitle}". ${
-                            this.target.toString()}`)}"
-                    target="_blank"
-                    rel="noopener"
-                    title="Share on Twitter">
-                ${twitterIcon}
-            </a>
+            <ul>
+                ${ifDefined(navigator.share && html`
+                    <li>
+                        <button id="share" @click="${this.onShare.bind(this)}"
+                                class="unstyled" title="Share">
+                            ${shareIcon}
+                        </button>
+                    </li>
+                `)}
+                ${ifDefined(navigator.clipboard?.writeText && html`
+                    <li>
+                        <button id="copy" @click="${this.onCopy.bind(this)}"
+                                class="unstyled" title="Copy to clipboard">
+                            ${copyIcon}
+                        </button>
+                    </li>
+                `)}
+                <li>
+                    <a href="https://twitter.com/intent/tweet?text=${
+                            encodeURIComponent(
+                                    `Check out: "${this.articleTitle}". ${
+                                        this.target.toString()}`)}"
+                            target="_blank"
+                            rel="noopener"
+                            title="Share on Twitter">
+                        ${twitterIcon}
+                    </a>
+                </li>
+            </ul>
         `;
     }
 
@@ -179,7 +204,7 @@ const copyIcon = html`
 `;
 
 const twitterIcon = html`
-    <svg id="twitter-logo">
+    <svg id="twitter-logo" viewBox="0 0 24 24">
         <title>Twitter logo</title>
         <g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g>
     </svg>
