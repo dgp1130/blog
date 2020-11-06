33de430dc0e4caa1975f7fbaefa8a5600ed43eda
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-11-06 15:50:38 -0500

    Chip through some situations to get storybook working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/client/components/*.stories.{js,md,mdx}'],
+  addons: [	
+    'storybook-prebuilt/addon-knobs/register.js',	
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  esDevServer: {
+    // custom es-dev-server options
+    nodeResolve: true,
+    watch: true,
+    open: true
+  },
+};
