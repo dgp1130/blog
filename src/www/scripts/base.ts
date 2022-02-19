1e37d7ecc0c2d3ee37c6ea6d17b88151dc44e9ab
Doug Parker <dgp1130@users.noreply.github.com>
2022-02-19 15:02:32 -0800

    Adds analytics to base page.

---
+++ src/www/scripts/base.ts
@@ -0,0 +1,5 @@
+import { trackAll } from '../../client/analytics';
+
+// Start analytics on the page. Run them for the lifetime of the page, so we
+// drop the "stop tracking" return value of this function.
+trackAll();
