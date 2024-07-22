89caa1f158dc6713eeae585540c8e330e0145b73
Doug Parker <dgp1130@users.noreply.github.com>
2024-07-21 18:41:16 -0700

    WIP: Migrates `share_test` to Vitest.

--- src/client/share.ts
+++ src/client/share.ts
@@ -1,16 +1,19 @@
-import { getLocation } from "./browser_env";
-
 /**
  * Returns a shareable URL for the given path. It uses the domain currently on
  * `window.location` with the provided path. It will also drop any query
  * parameters, hash values, or any other data inappropriate for sharing that
  * happens to be currently on `window.location`.
- * 
+ *
  * @param path An absolute path on the current domain (ie. `/foo/bar`).
  */
-export function makeShareable(path: string): URL {
+export function makeShareable(
+    path: string,
+    { getLocation = () => new URL(window.location.href) }: {
+        getLocation?: () => URL,
+    } = {},
+): URL {
     // Use a new URL with only the parts that we want. This drops query
     // params, hash values, etc. which may be inappropriate for sharing.
-    const pageUrl = new URL(getLocation().href);
+    const pageUrl = getLocation();
     return new URL(`${pageUrl.protocol}//${pageUrl.host}${path}`);
 }
--- src/client/share_test.ts
+++ src/client/share_test.ts
@@ -1,25 +1,20 @@
-import * as browserEnv from './browser_env';
+import { describe, expect, it } from 'vitest';
 import { makeShareable } from './share';
 
 describe('share', () => {
     describe('makeShareable()', () => {
         it('returns a URL from the current window origin', () => {
-            spyOn(browserEnv, 'getLocation').and.returnValue({
-                href: 'https://example.com/',
-            } as Location);
-
-            const url = makeShareable('/foo/bar/baz');
+            const url = makeShareable('/foo/bar/baz', {
+                getLocation: () => new URL('https://example.com/'),
+            });
 
             expect(url.toString()).toBe('https://example.com/foo/bar/baz');
         });
 
         it('returns a URL with non-shareable information santitized from the current window origin', () => {
-            spyOn(browserEnv, 'getLocation').and.returnValue({
-                // Most of this information isn't shareable.
-                href: 'https://user:pass@example.com:80/test?foo=bar#something',
-            } as Location);
-
-            const url = makeShareable('/foo/bar/baz');
+            const url = makeShareable('/foo/bar/baz', {
+                getLocation: () => new URL('https://user:pass@example.com:80/test?foo=bar#something'),
+            });
 
             expect(url.toString()).toBe('https://example.com:80/foo/bar/baz');
         });
