e94a9c4c6efc39c36d8ba115925aa1891ca35f72
Doug Parker <dgp1130@users.noreply.github.com>
2025-02-07 16:11:04 -0800

    Disable `collapseWhitespace`.
    Comment elaborates on context, but the HTML whitespace post intentionally depends on not collapsing whitespacing and there's no easy way to opt-it out. I quick check of the page indicates the benefit is minimal anyways, so I'm inclined ot just turn it off.
    
    If anything this just reinforces the point of that post that HTML is broken, given that my attempt at minification broke a blog post.

--- configs/html_minifier.js
+++ configs/html_minifier.js
@@ -6,7 +6,12 @@ const HtmlMinifier = require('html-minifier-terser');
 module.exports.htmlMinifierConfig = {
     caseSensitive: true,
     collapseBooleanAttributes: true,
-    collapseWhitespace: true,
+
+    // HTML Whitespace post, Example 31 depends on whitespace collapsing not
+    // happening. Also the size benefit of this seems minimal anyways (~126KB vs
+    // ~128KB) for that page. Therefore I have decided I don't care.
+    collapseWhitespace: false,
+
     decodeEntities: true,
     removeAttributeQuotes: true,
     removeComments: true,
