3f633c27f6faba85631731b08f6d2e2b328a020e
Doug Parker <dgp1130@users.noreply.github.com>
2026-06-21 22:09:08 -0700

    Add basic login and listing functionality to `publish-standard-site-documents` tool.

--- tools/publish-standard-site-documents/src/index.ts
+++ tools/publish-standard-site-documents/src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello!');
+import * as fs from 'node:fs/promises';
+import { AtpAgent } from '@atproto/api';
+
+const DOCUMENT_ROOT = '../../dist/standard-site-documents';
+const ATPROTO_PROVIDER = 'https://bsky.social';
+const ATPROTO_HANDLE = 'develwithoutacause.dwac.dev';
+const ATPROTO_PASSWORD = process.env['ATPROTO_PASSWORD'];
+
+async function main(): Promise<void> {
+    // Secret must be provided via environment variable.
+    if (!ATPROTO_PASSWORD) {
+        throw new Error('ATPROTO_PASSWORD environment variable is required.');
+    }
+
+    // Log in as the target user.
+    const agent = new AtpAgent({ service: ATPROTO_PROVIDER });
+    const res = await agent.login({
+        identifier: ATPROTO_HANDLE,
+        password: ATPROTO_PASSWORD,
+    })
+    if (!res.success) throw new Error('Login failed', {cause: res.data});
+
+    // Read and publish all documents.
+    const documents = await fs.readdir(DOCUMENT_ROOT);
+    for (const document of documents) {
+        const tid = document.slice(0, -'.json'.length);
+        const rkey = tid;
+        console.log(rkey);
+    }
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
